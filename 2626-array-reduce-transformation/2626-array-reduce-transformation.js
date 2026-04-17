c84b4dc12779652212ae0f485b28612220a511ad
JASEEM-SIDHEEQ-K <jaseemsidheeqk@gmail.com>
2026-04-17 14:41:07 +0530

    Time: 41 ms (79.37%), Space: 53.8 MB (72.78%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let out = init
+    for(let i=0; i<nums.length; i++){
+        out=fn(out,nums[i])
+    }
+    return out
+};
